3e497a7fd583f0933e4f975b3e7af5bc0fdffcd0
Mikepro <miguelalexander890@gmail.com>
2024-05-13 22:19:34 -0600

    Diseño_UI_Login

--- app/src/main/java/com/devy/mercurioapp/MainActivity.kt
+++ app/src/main/java/com/devy/mercurioapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.devy.mercurioapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       var btnSingIn = findViewById<Button>(R.id.btnSingIn)
+        btnSingIn.setOnClickListener {
+            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+            Log.i("MainActivity", "Button clicked")
+        }
     }
 }
